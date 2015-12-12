b2bc1c1fc3296bf1ee3f41ec77445e8343aa549d
Quoc-Anh Vu <quoc.v.anh@gmail.com>
2015-12-11 21:30:17 -0800

    Test: added new test

--- tests/unit/capitalize.js
+++ tests/unit/capitalize.js
@@ -1,5 +1,6 @@
 module.exports = function(subject, reporter) {
   reporter.describe("Unit Test: ''.capitalize()");
   reporter.assert(subject.capitalize, "test", "Test", "'Test' is capitalized");
+  reporter.assert(subject.capitalize, "thisIsATest", "ThisIsATest", "'ThisIsATest' is capitalized");
   reporter.finish();
 }
